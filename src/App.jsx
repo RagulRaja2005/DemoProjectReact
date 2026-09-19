f174f55fd72df59c00a70282b3db68b3caba4263
Ragul Raja <ragulsrs2005@gmail.com>
2026-09-19 12:56:35 +0530

    Add import GRoccery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MovieCounter from './Componets/MovieCounter'
+import GrocerryList from './Componets/GrocerryList'
 
 
 export default function App(){
 
   return(
-    <MovieCounter/>
+    // <MovieCounter/>
+    <GrocerryList/>
   )
 }
